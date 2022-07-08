4ea8f4268c64adccee99551d6b9ff65c338e2b09
Ilya Bogdanov <zeerayne1337@gmail.com>
2022-07-08 20:15:58 +0300

    `GitHubPublicPeerGatherer` was refactored a little bit

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import http.client
-import json
 import logging
 import os
 import re
-import select
 import subprocess
-import time
 from dataclasses import dataclass
 from glob import glob
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 import validators
+log = logging.Logger(__name__)
 
 
 @dataclass
@@ -20,7 +17,14 @@ class PeerData:
     address: str
     port: int
     params: str
-    raw: str
+    alive: Optional[bool] = None
+
+    @property
+    def raw(self) -> str:
+        return f'{self.proto}://{self.address}:{self.port}{self.params}'
+
+    def __hash__(self) -> int:
+        return hash(self.raw)
 
 
 class cd:
@@ -41,35 +45,22 @@ class cd:
         os.chdir(self.origin)
 
 
+def parse_peers(text: str, regex_string_suffux: str = '') -> List[PeerData]:
+    pattern = '(?P<proto>tcp|tls)://(?P<address>(?:[0-9.]+)|(?:\[[0-9a-fA-F:]+\])|(?:[A-Za-z0-9](?:[.A-Za-z0-9\-]{0,61}\.[A-Za-z0-9]{2,}))):(?P<port>[0-9]+)(?P<params>[?=&\w\d]*)'
+    pattern += regex_string_suffux
+    address_matches = re.finditer(pattern, text)
+    return [
+        PeerData(m.group('proto'),
+                 m.group('address'), int(m.group('port')), m.group('params'))
+        for m in address_matches
+    ]
+
+
 class GitHubPublicPeerGatherer:
     YGGDRASIL_PUBLIC_PEERS_REPO_DIR = 'public-peers'
     YGGDRASIL_PUBLIC_PEERS_REPO_URL = 'https://github.com/yggdrasil-network/public-peers.git'
 
-    # filter params
-    only_tcp = None
-    only_tls = None
-    only_ipv4 = None
-    only_ipv6 = None
-    prefer_tcp = None
-    prefer_tls = None
-
-    def __init__(
-        self,
-        only_tcp: bool = False,
-        only_tls: bool = False,
-        only_ipv4: bool = False,
-        only_ipv6: bool = False,
-        prefer_tcp: bool = False,
-        prefer_tls: bool = False,
-    ) -> None:
-        self.only_tcp = only_tcp
-        self.only_tls = only_tls
-        self.only_ipv4 = only_ipv4
-        self.only_ipv6 = only_ipv6
-        self.prefer_tcp = prefer_tcp
-        self.prefer_tls = prefer_tls
-
-    def _execute_shell_command(command) -> int:
+    def _execute_shell_command(self, command) -> int:
         proc = subprocess.Popen(command.split(), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
         proc.wait()
         return proc.returncode
@@ -83,45 +74,41 @@ class GitHubPublicPeerGatherer:
             exit_code = self._execute_shell_command(cmd)
         return exit_code == 0
 
-    def _parse_peers(self, text) -> PeerData:
-        address_matches = re.finditer(
-            r'(?P<proto>tcp|tls)://\
-            (?P<address>(?:[0-9.]+)|(?:\[[0-9a-fA-F:]+\])|(?:[A-Za-z0-9](?:[.A-Za-z0-9\-]{0,61}\.[A-Za-z0-9]{2,}))):\
-            (?P<port>[0-9]+)(?P<params>[?=\w\d]*)\`', text
-        )
-        return [
-            PeerData(
-                m.group('proto'),
-                m.group('address').strip('[]'), int(m.group('port')), m.group('params'), m.string
-            ) for m in address_matches
-        ]
-
     def _parse_peers_file(self, file_path) -> PeerData:
-        with open(file_path) as fhandle:
+        with open(file_path, encoding='utf-8') as fhandle:
             filecontent = fhandle.read()
-        return self._parse_peers(filecontent)
+        return parse_peers(filecontent, r'`')
 
-    def get_peers(self) -> List[PeerData]:
+    def get_peers(
+        self,
+        only_tcp: bool = False,
+        only_tls: bool = False,
+        only_ipv4: bool = False,
+        only_ipv6: bool = False
+    ) -> List[PeerData]:
+
         def _validate_peer(peer: PeerData) -> bool:
             valid = True
-            valid &= any(
-                validators.ip_address.ipv4(peer.address), validators.ip_address.ipv6(peer.address),
+            valid &= any([
+                validators.ip_address.ipv4(peer.address),
+                validators.ip_address.ipv6(peer.address.strip('[]')),
                 validators.domain(peer.address)
-            )
+            ])
             valid &= validators.between(peer.port, min=1, max=65535)
             return valid
 
         def _filter_peer(peer: PeerData) -> bool:
             valid = True
-            valid &= not self.only_tcp or peer.proto == 'tcp'
-            valid &= not self.only_tls or peer.proto == 'tls'
-            valid &= not self.only_ipv4 or validators.ip_address.ipv4(peer.address)
-            valid &= not self.only_ipv6 or validators.ip_address.ipv6(peer.address)
+            valid &= not only_tcp or peer.proto == 'tcp'
+            valid &= not only_tls or peer.proto == 'tls'
+            valid &= not only_ipv4 or validators.ip_address.ipv4(peer.address)
+            valid &= not only_ipv6 or validators.ip_address.ipv6(peer.address.strip('[]'))
             return valid
-            
+
         self._prepare_repo()
         peers = []
         for peer_file in glob(f'{self.YGGDRASIL_PUBLIC_PEERS_REPO_DIR}/*/*.md'):
             peers += self._parse_peers_file(peer_file)
-        
+
         return list(filter(_filter_peer, filter(_validate_peer, peers)))
+
